266253d68d9933a4176c330aef86d8d41ea83709
Chris Turina and Kimberly Bordon <kimberly@>
2017-09-25 09:32:11 -0700

    Add fourth spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //business logic
-function Place(name, date, landmark) {
+function Place(name, date, landmark, notes) {
   this.placeName = name;
   this.placeDate = date;
   this.placeLandmark = landmark;
+  this.placeNotes = notes;
 }
 //user interface logic
 $(document).ready(function() {
@@ -12,8 +13,9 @@ $(document).ready(function() {
     var inputtedPlaceName = $("input#place-name").val();
     var inputtedPlaceDate = $("input#place-date").val();
     var inputtedPlaceLandmark = $("input#place-landmark").val();
+    var inputtedPlaceNotes = $("textarea#place-notes").val();
 
-    var newPlace = new Place(inputtedPlaceName, inputtedPlaceDate, inputtedPlaceLandmark);
+    var newPlace = new Place(inputtedPlaceName, inputtedPlaceDate, inputtedPlaceLandmark, inputtedPlaceNotes);
 
     $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
 
@@ -23,10 +25,12 @@ $(document).ready(function() {
       $(".place-name").text(newPlace.placeName);
       $(".place-date").text(newPlace.placeDate);
       $(".place-landmark").text(newPlace.placeLandmark);
+      $(".place-notes").text(newPlace.placeNotes);
     });
 
     $("input#place-name").val("");
     $("input#place-date").val("");
     $("input#place-landmark").val("");
+    $("input#place-notes").val("");
   })
 });
