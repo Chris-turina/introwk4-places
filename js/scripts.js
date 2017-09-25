958a4f3e3a6cc0499d40e6fadb27d11540e956ac
Chris Turina and Kimberly Bordon <kimberly@>
2017-09-25 09:18:13 -0700

    Add third spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //business logic
-function Place(name, date) {
+function Place(name, date, landmark) {
   this.placeName = name;
   this.placeDate = date;
+  this.placeLandmark = landmark;
 }
 //user interface logic
 $(document).ready(function() {
@@ -10,8 +11,9 @@ $(document).ready(function() {
 
     var inputtedPlaceName = $("input#place-name").val();
     var inputtedPlaceDate = $("input#place-date").val();
+    var inputtedPlaceLandmark = $("input#place-landmark").val();
 
-    var newPlace = new Place(inputtedPlaceName, inputtedPlaceDate);
+    var newPlace = new Place(inputtedPlaceName, inputtedPlaceDate, inputtedPlaceLandmark);
 
     $("ul#places").append("<li><span class='place'>" + newPlace.placeName + "</span></li>");
 
@@ -20,9 +22,11 @@ $(document).ready(function() {
       $("#show-place h2").text(newPlace.placeName);
       $(".place-name").text(newPlace.placeName);
       $(".place-date").text(newPlace.placeDate);
+      $(".place-landmark").text(newPlace.placeLandmark);
     });
 
     $("input#place-name").val("");
     $("input#place-date").val("");
+    $("input#place-landmark").val("");
   })
 });
